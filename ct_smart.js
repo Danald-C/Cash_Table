be09fed4a65b3ae2f661cc62a351c6a7702e1d08
danald <kwadjodanq@gmail.com>
2025-05-08 18:33:12 -0800

    More restructing, added Totals & Sorting.

--- ct_smart.js
+++ ct_smart.js
@@ -1,8 +1,8 @@
 $(function(){
 	let all_tables = [];
 	// let sel_state = [-1, [3, [0, 0]]];
-	// let sel_state = {"table": -1, "controls": {"columnCount": 3, "rowAndcolumn": [0, 0]}};
-	let sel_state = {"table": -1, "controls": {"columnCount": 0, "rowAndcolumn": [0, -1]}};
+	// let sel_state = {"table": -1, "controls": {"columnCount": 0, "rowAndcolumn": [0, -1]}};
+	let sel_state = {"table": -1, "controls": {"columnCount": 0, "rowAndcolumn": resetRowAndColumn([0, [-1, []]])}};
 	$("div#set-2, div#set-3, div#set-3 div#opt-2, div#set-4").css("display", "none");
 	$("div#set-1 input#cash-table-name").val("").focus();
 	
@@ -105,18 +105,20 @@ $(function(){
 		$("div#set-4 input#cell-data").val("");
 		let column_name = $(this).closest('div').find('input#cash-column-name');
 		
-		if(sel_state.controls.rowAndcolumn[1] >= 0){
+		if(sel_state.controls.rowAndcolumn[1][0] >= 0){
 			if(column_name.val() != ""){
-				all_tables[sel_state.table][1].forEach((row, i) => { // Each row
-					// console.log(all_tables[sel_state.table][1][i]);
-					all_tables[sel_state.table][1][i][4][sel_state.controls.rowAndcolumn[1]][0] = column_name.val();
-				});
+				/* all_tables[sel_state.table][1].forEach((row, i) => { // Each row
+					all_tables[sel_state.table][1][i][4][sel_state.controls.rowAndcolumn[1][0]][0] = column_name.val();
+				}); */
+				sel_state.controls.rowAndcolumn[1][1][sel_state.controls.rowAndcolumn[1][0]] = column_name.val();
 				// $('div#set-3 div#opt-2').css('display', 'none');
 			}
 		}else{
-			sel_state.controls.rowAndcolumn = resetRowAndColumn();
-			// all_tables[sel_state.table] = column_processor(all_tables[sel_state.table], sel_state.controls.columnCount);
-			all_tables[sel_state.table] = column_processor(all_tables[sel_state.table], column_name.val());
+			sel_state.controls.rowAndcolumn = resetRowAndColumn(sel_state.controls.rowAndcolumn);
+			// all_tables[sel_state.table] = column_processor(all_tables[sel_state.table], column_name.val());
+			let [sel_table, rowAndcolumn] = column_processor([all_tables[sel_state.table], sel_state.controls.rowAndcolumn], column_name.val());
+			all_tables[sel_state.table] = sel_table;
+			sel_state.controls.rowAndcolumn = rowAndcolumn;
 			column_name.val("");
 		}
 		
@@ -129,18 +131,21 @@ $(function(){
 			$("div#set-4 input#cell-data").val("");
 			// let getInput = $(this).closest('div').find('input');
 			
-			if(sel_state.controls.rowAndcolumn[1] >= 0){
+			if(sel_state.controls.rowAndcolumn[1][0] >= 0){
 				if($(this).val() != ""){
-					all_tables[sel_state.table][1].forEach((row, i) => { // Each row
+					/* all_tables[sel_state.table][1].forEach((row, i) => { // Each row
 						// console.log(all_tables[sel_state.table][1][i]);
-						all_tables[sel_state.table][1][i][4][sel_state.controls.rowAndcolumn[1]][0] = $(this).val();
-					});
+						all_tables[sel_state.table][1][i][4][sel_state.controls.rowAndcolumn[1][0]][0] = $(this).val();
+					}); */
+					sel_state.controls.rowAndcolumn[1][1][sel_state.controls.rowAndcolumn[1][0]] = $(this).val();
 					// $('div#set-3 div#opt-2').css('display', 'none');
 				}
 			}else{
-				sel_state.controls.rowAndcolumn = resetRowAndColumn();
-				// all_tables[sel_state.table] = column_processor(all_tables[sel_state.table], sel_state.controls.columnCount);
-				all_tables[sel_state.table] = column_processor(all_tables[sel_state.table], $(this).val());
+				sel_state.controls.rowAndcolumn = resetRowAndColumn(sel_state.controls.rowAndcolumn);
+				// all_tables[sel_state.table] = column_processor(all_tables[sel_state.table], $(this).val());
+				let [sel_table, rowAndcolumn] = column_processor([all_tables[sel_state.table], sel_state.controls.rowAndcolumn], $(this).val());
+				all_tables[sel_state.table] = sel_table;
+				sel_state.controls.rowAndcolumn = rowAndcolumn;
 				$(this).val("");
 			}
 			// console.log(all_tables[sel_state.table]);
@@ -154,28 +159,41 @@ $(function(){
 
 		all_tables[sel_state.table][1].forEach((row, i) => { // Each row
 			// console.log(all_tables[sel_state.table][1][i]);
-			all_tables[sel_state.table][1][i][4].splice(sel_state.controls.rowAndcolumn[1], 1); // Remove the selected column
+			all_tables[sel_state.table][1][i][4].splice(sel_state.controls.rowAndcolumn[1][0], 1); // Remove the selected column
 		});
+		sel_state.controls.rowAndcolumn[1][1].splice(sel_state.controls.rowAndcolumn[1][0], 1); // Remove the selected column
 		
-		sel_state.controls.rowAndcolumn[1] = -1;
+		sel_state.controls.rowAndcolumn[1][0] = -1;
 		sel_state = process_table(all_tables[sel_state.table], sel_state);
 	});
 	
 	// UPDATE A CELL
 	$("div#set-4 a").click(function(){
-		let row = sel_state.controls.rowAndcolumn[0];
-		let cell = sel_state.controls.rowAndcolumn[1];
-		all_tables[sel_state.table][1][row][cell] = $("div#set-4 input#cell-data").val();
-		sel_state = process_table(all_tables[sel_state.table], sel_state);
-		$("div#set-4 input#cell-data").val("").focus();
+		let input_elem = $("div#set-4 input#cell-data");
+		if(input_elem.val() !== ""){
+			let row = sel_state.controls.rowAndcolumn[0];
+			let cell = sel_state.controls.rowAndcolumn[1][0];
+			// all_tables[sel_state.table][1][row][cell] = $("div#set-4 input#cell-data").val();
+			all_tables[sel_state.table][1][row][4][cell] = input_elem.val();
+			
+			sel_state.controls.rowAndcolumn[0] = 0;
+			sel_state.controls.rowAndcolumn[1][0] = -1;
+			// $("div#set-4 input#cell-data").val("").focus();
+			input_elem.val("");
+			sel_state = process_table(all_tables[sel_state.table], sel_state);
+		}
 	});
 	$("div#set-4 input#cell-data").keyup(function(e){
-		if(e.key == "Enter"){
+		if(e.key == "Enter" && $(this).val() !== ""){
 			let row = sel_state.controls.rowAndcolumn[0];
-			let cell = sel_state.controls.rowAndcolumn[1];
-			all_tables[sel_state.table][1][row][cell] = $(this).val();
+			let cell = sel_state.controls.rowAndcolumn[1][0];
+			all_tables[sel_state.table][1][row][4][cell] = $(this).val();
+			
+			sel_state.controls.rowAndcolumn[0] = 0;
+			sel_state.controls.rowAndcolumn[1][0] = -1;
+			// $(this).val("").focus();
+			$(this).val("");
 			sel_state = process_table(all_tables[sel_state.table], sel_state);
-			$(this).val("").focus();
 		}
 	});
 });
@@ -193,19 +211,25 @@ $(function(){
 	}
 	return sel_table;
 } */
-function column_processor(sel_table, column_name=""){
-	let allRows = sel_table[1];
+// function column_processor(sel_table, column_name=""){
+function column_processor(data, column_name=""){
+	// let allRows = sel_table[1];
+	let allRows = data[0][1];
 	if(allRows.length > 0){
 		for(var i=0; i<allRows.length; i++){ // Loop through all rows
-			sel_table[1][i][4].push([column_name, 0]); // Add new column with 0 value. Index 4 is the columns array
+			// sel_table[1][i][4].push([column_name, 0]); // Add new column with 0 value. Index 4 is the columns array
+			data[0][1][i][4].push(0); // Add new column with 0 value. Index 4 is the columns array
 		}
-
+		data[1][1][1].push(column_name);
+		
 		$('div#set-4').css('display', 'none');
-		if(sel_table[1][0][4].length > 0){
+		// if(sel_table[1][0][4].length > 0){
+		if(data[0][1][0][4].length > 0){
 			$('div#set-4').css('display', 'block');
 		}
 	}
-	return sel_table;
+	// return sel_table;
+	return data;
 }
 
 function process_table(sel_table, sel_state){
@@ -218,16 +242,21 @@ function process_table(sel_table, sel_state){
 
 		firstRow[4].map(col => eachColumnsTotal[1].push(0)); // Create a base value for the other columns
 		sel_table[1].map((row, i) => { // Each row
+			sel_table[1][i][3] = 0; // Reset before adding up.
 			row[4].map((cell, j) => { // Each item in index 4, which is the columns array
-				let cell_num = Number(cell[1]);
-				if(!isNaN(cell_num)){
+				// let cell_num = Number(cell[1]);
+				let cell_num = cell;
+				// if(!isNaN(cell_num)){
 					eachColumnsTotal[1][j] = sum_1([eachColumnsTotal[1][j], cell_num], 2); // Get the total sum-up of each column.
 					sel_table[1][i][3] = sum_1([sel_table[1][i][3], cell_num], 2); // Add up all the numbers into total column
-				}
+					// console.log("i: "+i, ", j: "+j, ", Cell: ", cell_num, ", Total: "+sel_table[1][i][3]);
+				// }
 			})
 		})
+		eachColumnsTotal[1].map(cell => eachColumnsTotal[0] = sum_1([eachColumnsTotal[0], cell], 2));
 
-		let get_pos = function(allRows, new_data=[[], [], [-1, 0]], n=[[0, 1], [], 0]){
+		// let get_pos = function(allRows, new_data=[[], [], [-1, 0]], n=[[0, 1], [], 0]){
+		let get_pos = function(allRows, total, new_data=[[], [], [-1, 0]], n=[[0, 1], [], 0]){
 			if(new_data[0].length < allRows.length){
 				if(new_data[1].length == 0){ // On the very first call
 					for(let i=0; i<allRows.length; i++){
@@ -235,14 +264,16 @@ function process_table(sel_table, sel_state){
 					}
 				}
 				let this_i_1 = new_data[1][n[0][0]]; // Get a row's index but on the very first call, get the first row
-				let curr_amount = allRows[this_i_1][3]; // Column called total of that row
+				// let curr_amount = allRows[this_i_1][3]; // Column called total of that row
+				let curr_amount = total >= 0 ? allRows[this_i_1][4][total] : allRows[this_i_1][3];
 				
 				// Compare current & next row's amount
 				if(n[2] == 0){
 					// n[0][0] & n[0][1] is getting the current & next row's index
 					if(new_data[1].length > 1 && n[0][1] < new_data[1].length){ // Proceed if size is larger than the first current & next row
 						let this_i_2 = new_data[1][n[0][1]]; // Get the next row's index
-						let nxt_amount = allRows[this_i_2][3]; // Column called total of that row
+						// let nxt_amount = allRows[this_i_2][3]; // Column called total of that row
+						let nxt_amount = total >= 0 ? allRows[this_i_2][4][total] : allRows[this_i_2][3];
 						
 						if(curr_amount >= nxt_amount){ // Greater or Equal
 							n[0][1] += 1; // Increment to the next row index
@@ -278,7 +309,8 @@ function process_table(sel_table, sel_state){
 					// Once 1 iteration is successfully done here, switch to the previous block.
 				}
 				
-				return get_pos(allRows, new_data, n);
+				// return get_pos(allRows, new_data, n);
+				return get_pos(allRows, total, new_data, n);
 			}else{
 				for(let i=0; i<new_data[0].length; i++){
 					new_data[0][i][0] = i+1; // Number rows
@@ -288,7 +320,8 @@ function process_table(sel_table, sel_state){
 				return allRows;
 			}
 		}
-		sel_table[1] = get_pos(sel_table[1]);
+		// sel_table[1] = get_pos(sel_table[1]);
+		sel_table[1] = get_pos(sel_table[1], sel_state.controls.rowAndcolumn[1][0]);
 
 		// # TABLE MAKE-UP
 		// Head/Caption row
@@ -306,11 +339,12 @@ function process_table(sel_table, sel_state){
 				stat_struct += "<strong>Total</strong>";
 			stat_struct += "</div>";
 			firstRow[4].map((col, i) => {
-				stat_struct += "<div class='column grps-data column-cap ";
-				stat_struct += sel_state.controls.rowAndcolumn[1] == i ? "selected" : "";
+				stat_struct += "<div class='column grps-data column-cap";
+				stat_struct += sel_state.controls.rowAndcolumn[1][0] == i ? " selected" : "";
 				stat_struct += "'>";
 					stat_struct += "<strong>";
-						stat_struct += col[0] == "" ? "Cap. "+(i+1) : col[0];
+						// stat_struct += col[0] == "" ? "Cap. "+(i+1) : col[0];
+						stat_struct += sel_state.controls.rowAndcolumn[1][1][i] == "" ? "Cap. "+(i+1) : sel_state.controls.rowAndcolumn[1][1][i];
 					stat_struct += "</strong>";
 				stat_struct += "</div>";
 			});
@@ -323,15 +357,17 @@ function process_table(sel_table, sel_state){
 			stat_struct += "<div class='column grps-names'></div>";
 			stat_struct += "<div class='column grps-total grps-data'><strong>"+filter_currency(eachColumnsTotal[0])+"</strong></div>";
 			firstRow[4].map((col, i) => {
-				stat_struct += "<div class='column grps-data ";
-				stat_struct += sel_state.controls.rowAndcolumn[1] == i ? "sel-column" : "";
+				stat_struct += "<div class='column grps-data";
+				stat_struct += sel_state.controls.rowAndcolumn[1][0] == i ? " sel-column" : "";
 				stat_struct += "'>";
-					stat_struct += "<strong>"+filter_currency(eachColumnsTotal[col[1]])+"</strong>";
+					// stat_struct += "<strong>"+filter_currency(eachColumnsTotal[col[1]])+"</strong>";
+					stat_struct += "<strong>"+filter_currency(eachColumnsTotal[1][i])+"</strong>";
 				stat_struct += "</div>";
 			});
 		stat_struct += "</li>";
 		// console.log(stat_struct)
 
+		// Every other row or the tables actual body. Subsequent body rows.
 		sel_table[1].map((row, i) => { // Each row
 			stat_struct += "<li id='"+i+"' class='row grps-body'>";
 				stat_struct += "<div class='column grps-num'>"+row[0]+"</div>";
@@ -339,10 +375,20 @@ function process_table(sel_table, sel_state){
 				stat_struct += "<div class='column grps-names'>"+row[2]+"</div>";
 				stat_struct += "<div class='column grps-total grps-data'><strong>"+filter_currency(row[3])+"</strong></div>";
 				row[4].map((cell, j) => { // Each item in index 4, which is the columns array
-					stat_struct += "<div class='column grps-data ";
-					stat_struct += sel_state.controls.rowAndcolumn[1] == j ? "sel-column" : "";
+					stat_struct += "<div class='column grps-data";
+					if(sel_state.controls.rowAndcolumn[1][0] == j){
+						if(sel_state.controls.rowAndcolumn[0] < 2){
+							stat_struct += " sel-column";
+						}else if(sel_state.controls.rowAndcolumn[0] == i+2){
+							stat_struct += " selected";
+						}else{
+							stat_struct += "";
+						}
+					}
+					// stat_struct += sel_state.controls.rowAndcolumn[1][0] == j && sel_state.controls.rowAndcolumn[0] < 2 ? "sel-column" : "";
 					stat_struct += "'>";
-						stat_struct += "<span>"+filter_currency(eachColumnsTotal[cell[1]])+"</span>";
+						// stat_struct += "<span>"+filter_currency(eachColumnsTotal[cell[1]])+"</span>";
+						stat_struct += "<span>"+filter_currency(cell)+"</span>";
 					stat_struct += "</div>";
 				})
 			stat_struct += "</li>";
@@ -353,15 +399,23 @@ function process_table(sel_table, sel_state){
 	$("ul#set-1").empty().html(stat_struct);
 	
 	$("ul#set-1 li.grps-body div.grps-data").click(function(){
-		if($(this).attr("id") > 3){
-			$("ul#set-1 li div.grps-data").removeClass("selected");
-			$(this).addClass("selected");
-			let row = $(this).closest("li").attr("id"), cell = $(this).attr("id");
-			
-			sel_state.controls.rowAndcolumn[0] = row;
-			sel_state.controls.rowAndcolumn[1] = cell;
-			let sel_cell = all_tables[1][row][cell];
-			$("div#set-4 input#cell-data").val(sel_cell).focus();
+		// if($(this).attr("id") > 3){
+		if($(this).index() > 3){
+			if(!$(this).is('.selected')){
+				$("ul#set-1 li div.grps-data").removeClass("selected");
+				$(this).addClass("selected");
+				// let row = $(this).closest("li").attr("id"), cell = $(this).attr("id");
+				let row = $(this).closest("li.grps-body").index()-2, cell = $(this).index()-4;
+				
+				sel_state.controls.rowAndcolumn[0] = row;
+				sel_state.controls.rowAndcolumn[1][0] = cell;
+				let sel_cell = sel_table[1][row][4][cell];
+				$("div#set-4 input#cell-data").val(parseFloat(sel_cell)).focus();
+			}else{
+				$("ul#set-1 li div.grps-data").removeClass("selected");
+				$("div#set-4 input#cell-data").val("");
+				sel_state.controls.rowAndcolumn[1][0] = -1;
+			}
 		}
 	});
 
@@ -387,7 +441,8 @@ function process_table(sel_table, sel_state){
 			$('div#set-3 div#opt-2').css('display', 'none');
 			
 			$("div#set-3 div#opt-1 input#cash-column-name").val("");
-			sel_state.controls.rowAndcolumn[1] = -1;
+			sel_state.controls.rowAndcolumn[1][0] = -1;
+			sel_state = process_table(sel_table, sel_state);
 		}else{ // Add selected column's class
 			$('ul#set-1 li.grps-head div.column-cap').removeClass("selected");
 			$("ul#set-1 li.row div.column").removeClass("sel-column");
@@ -395,13 +450,16 @@ function process_table(sel_table, sel_state){
 			$(this).addClass("selected");
 			selectedColumn($(this), true);
 			$('div#set-3 div#opt-2').css('display', 'block');
-			sel_state.controls.rowAndcolumn[1] = $(this).index()-4; // Get the index of the column cap - 4 because of the first 3 columns (S/N, Pos. & Name)
+			sel_state.controls.rowAndcolumn[1][0] = $(this).index()-4; // Get the index of the column cap - 4 because of the first 3 columns (S/N, Pos. & Name)
 			
 			$("div#set-3 div#opt-1 input#cash-column-name").val("").focus();
-			if(sel_table[1][0][4][sel_state.controls.rowAndcolumn[1]][0] !== ""){
-				$("div#set-3 div#opt-1 input#cash-column-name").val(sel_table[1][0][4][sel_state.controls.rowAndcolumn[1]][0]).focus();
+			// if(sel_table[1][0][4][sel_state.controls.rowAndcolumn[1][0]][0] !== ""){
+			if(sel_state.controls.rowAndcolumn[1][1][sel_state.controls.rowAndcolumn[1][0]] !== ""){
+				// $("div#set-3 div#opt-1 input#cash-column-name").val(sel_table[1][0][4][sel_state.controls.rowAndcolumn[1][0]][0]).focus();
+				$("div#set-3 div#opt-1 input#cash-column-name").val(sel_state.controls.rowAndcolumn[1][1][sel_state.controls.rowAndcolumn[1][0]]).focus();
 			}
 			// console.log(sel_state.controls.rowAndcolumn, sel_table[1]);
+			sel_state = process_table(sel_table, sel_state);
 		}
 	});
 	
@@ -410,10 +468,10 @@ function process_table(sel_table, sel_state){
 
 function sum_1(data, mode){
 	if(mode == 2){
-		return data[0] + data[1];
+		return parseFloat(data[0]) + parseFloat(data[1]);
 	}
 	if(mode == 1){
-		return data[0] * data[1];
+		return parseFloat(data[0]) * parseFloat(data[1]);
 	}
 }
 
@@ -487,6 +545,7 @@ function selectedColumn(col_cap, selStates=true){
 	}
 }
 
-function resetRowAndColumn(){
-	return [0, -1];
+function resetRowAndColumn(rowAndcolumn){
+	// return [0, -1];
+	return [0, [-1, rowAndcolumn[1][1].length > 0 ? rowAndcolumn[1][1] : []]];
 }
